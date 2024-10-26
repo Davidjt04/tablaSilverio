dcc3a4e93caf7a66312013adb202130d522a3c10
Davidjt04 <dajantor@gmail.com>
2024-10-26 13:05:38 +0200

    cargarDessdeCsv
    Plateamiento del cargar Csv

--- tablaCompleta/js/tabla.js
+++ tablaCompleta/js/tabla.js
@@ -1,45 +1,74 @@
-//Metodos (prototipos) de tablas
-HTMLTableElement.prototype.leerJson = function(json){
-    //DUDAS
-    //tengo que validar el json en caso de que lo tenga que hacer 
-        //Si valido el Json en formato Json tenfo que darle una estructura
-        //Si valido el Json en cadena solamente tengo que pasarlo a json para saber si la estructura es correcta o no 
-    //leo el json 
-            // var json = 
-    //compruebo el formato 
-    //devuelvo el json 
-    //JSON.stringify()->json a string   JSON.parse()->string a Json
-    //FORMA EN QUE CREO QUE SE HACE 
-    /*Cojo el json lo meto en un array de objetos esos objetos los voy metiendo junto con sus propiedades en las
-    filas de mi tabla*/
-console.log('cambio2');
-}
+// //Metodos (prototipos) de tablas
+// HTMLTableElement.prototype.leerJson = function(json){
+//     //DUDAS
+//     //tengo que validar el json en caso de que lo tenga que hacer 
+//         //Si valido el Json en formato Json tenfo que darle una estructura
+//         //Si valido el Json en cadena solamente tengo que pasarlo a json para saber si la estructura es correcta o no 
+//     //leo el json 
+//             // var json = 
+//     //compruebo el formato 
+//     //devuelvo el json 
+//     //JSON.stringify()->json a string   JSON.parse()->string a Json
+//     //FORMA EN QUE CREO QUE SE HACE 
+//     /*Cojo el json lo meto en un array de objetos esos objetos los voy metiendo junto con sus propiedades en las
+//     filas de mi tabla*/
+// console.log('cambio2');
+// }
 
 
-HTMLTableElement.prototype.traerJson = function(json){
-    //como meter los datos en la talbla
-        //coger la tabla
-        //coger las claves del json 
-        //ir creando tr y td e ir metiendo los datos
+// HTMLTableElement.prototype.traerJson = function(json){
+//     //como meter los datos en la talbla
+//         //coger la tabla
+//         //coger las claves del json 
+//         //ir creando tr y td e ir metiendo los datos
         
-}
-console.log("hola caracolaaaaa");
+// }
+// console.log("hola caracolaaaaa");
 
+
+
+// htmlcreateElement.prototype.cargarFichero = function(){
+    
+// }
+
+
+
+// htmlcreateElement.prototype.leerFichero = function(){
+    
+// }
+htmlcreateElement.prototype.guardarLocalStorage = function(){
+    //saber que datos le pasabamos yu como porque mediante formulario no fue 
+}
 
 htmlcreateElement.prototype.leerLocalStorage = function(){
     
-}
-
-htmlcreateElement.prototype.traerLocalStorage = function(){
+    //Leer los datos del LocalStorage:
+        //LocalStorage pasa los datos en txt por lo tanto tengo que pasarlos a json 
+    //
     
 }
 
-htmlcreateElement.prototype.guardarLocalStorage = function(){
+htmlcreateElement.prototype.leerServidor = function(){
+        //Leer los datos del LocalStorage:
+        //LocalStorage pasa los datos en txt por lo tanto tengo que pasarlos a json 
+    //
     
 }
 
 
-htmlcreateElement.prototype.leerFichero = function(){
-    
-}
+htmlcreateElement.prototype.cargarDesdeCsv = function(){
+    //coje el boton, mete el archivo, mira si hay o no archivo 
+    /*si ahy archivo se crea el objeto para leer y despues antes de leerlo se espera a que
+    cargaue la pagina para que el archivo este entero por que puede tener imagenes  u otras cosas 
+    que tarden en cargar la tabla tabien tendra que ir dentro de este onload*/
+    //ACCIONES PARA HACER LA TABLA DINAMICAMENTE 
+    //guarda la linea haciendo splits y tambien guarda los datos ya separados
+    //cuadno se van a introducir ya los datos se limpuia primero la tabla y ya despues se meten dentro
+    //crea el encabezado de la tabla con bucles 
+    //crea las filas de la tabla con bucles 
+    //finalmente se lee el archivo que ya va ir fuera del onload 
+        /*crea flujo 1-> objeto readers 
+                     2-> onload 
+                     3-> leemos el archivo*/
 
+}
